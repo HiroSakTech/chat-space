45b5d6b87dd494e2b3b07c080c6f97ccf97c4f00
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-05-27 19:24:49 +0900

    add edit update function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'groups/new'
-  get 'groups/create'
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
