3a130e58e6114a573052786f3b28e9eda566493c
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-05-27 17:13:56 +0900

    add new and create actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'groups/new'
+  get 'groups/create'
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
 end
