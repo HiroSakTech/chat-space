4341ba1c51934431ce9cd339850b6360c4746820
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-06-03 20:44:24 +0900

    add message-create-json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,62 @@
 $(function() {
+
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="Message-details">
+                    <div class="Message-details__info">
+                      <div class="Message-details__info--who">
+                        ${message.user_name}
+                      </div>
+                      <div class="Message-details__info--when">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="Message-details__content">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                      <img class="Message__image" src="${message.image}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = 
+                  `<div class="Message-details">
+                    <div class="Message-details__info">
+                      <div class="Message-details__info--who">
+                        ${message.user_name}
+                      </div>
+                      <div class="Message-details__info--when">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="Message-details__content">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('.Message-form__contents').on('submit',function(e) {
-    e.preventDefault();
+    e.preventDefault()
     let url = $(this).attr('action')
     let formData = new FormData(this);
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);   
+      $('.Message-field').append(html); 
+      $('form')[0].reset();
+      console.log(this);
     })
   })
 })
